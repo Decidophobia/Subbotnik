ea23e4ad60ce263f5beabd490527e0d93cfca43d
Tanya <tearfulta@gmail.com>
2020-11-20 16:17:05 +0300

    dotenv, atlas db connect, edited models

--- server/models/User.js
+++ server/models/User.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 
 const userShema = Schema({
-	email: { type: String, require: true },
-	password: { type: String, require: true },
-	avatar: String,
-	bio: String,
+	email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  name: {type: String, require: true},
+  phone: {type: Number, require: true, unique: true},
 });
 
 module.exports = mongoose.model("User", userShema);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const config = require('./middleware');
 config(app);
 
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4000;
 
 async function startServer() {
 	try {
-		await mongoose.connect('mongodb://localhost/reactauth', {
+		await mongoose.connect(`mongodb+srv://cleaner:${process.env.DB_PASSWORD}@subbotnic.ldlmg.mongodb.net/test`, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});

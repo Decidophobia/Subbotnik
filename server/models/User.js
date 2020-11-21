60490dbc235167762730e7f657a0dd75d4d2513b
Tanya <tearfulta@gmail.com>
2020-11-21 15:10:45 +0300

    model of chat, added fields to user model

--- server/models/User.js
+++ server/models/User.js
@@ -7,6 +7,8 @@ const userShema = Schema({
   password: { type: String, require: true },
   name: {type: String, require: true},
   phone: {type: Number, require: true, unique: true},
+  room: {type: Schema.Types.ObjectId, ref: 'Chat' },
+  pollutionRef: [ { type: Schema.Types.ObjectId, ref: 'Pollution' }]
 });
 
 module.exports = mongoose.model("User", userShema);

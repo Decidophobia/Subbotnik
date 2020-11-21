fc938aba9eb12246a5f308f727ff1e5677dcd171
SerSu <sersurgut88@yandex.ru>
2020-11-21 17:20:06 +0300

    chat basic version

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import socket from "../../socket";
+import { useDispatch, useSelector } from "react-redux";
+import { sendMessageAC } from "../../redux/actionCreators";
+import Message from "./Message";
+
 function Chat(props) {
+  const [message, setMessage] = useState("");
+  const dispatch = useDispatch();
+  const chat = useSelector((store) => store.chat.messages);
+
+  const messageText = (e) => {
+    setMessage(e.target.value);
+  };
+  const sendMessage = () => {
+    dispatch(sendMessageAC(message));
+    socket.emit("NEW_MESSAGE", message);
+    setMessage("");
+  };
+  useEffect(() => {
+    socket.on("NEW_MESSAGE:CLIENT", (message) => {
+      dispatch(sendMessageAC(message));
+    });
+  }, []);
   return (
     <div className="chat-container">
-      <div className="chat-field"></div>
+      <div className="chat-field">
+        {chat &&
+          chat.map((oneMes, i) => <Message oneMes={oneMes} key={oneMes + i} />)}
+      </div>
       <div className="chat-massage">
-        <input name="massage-input" />
-        <button className="chat-button">отправить</button>
+        <input name="massage-input" value={message} onChange={messageText} />
+        <button className="chat-button" onClick={sendMessage}>
+          отправить
+        </button>
       </div>
     </div>
   );
---
+++ client/src/Components/Chat/Message.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Message({ oneMes }) {
+
+  return <div className="oneMessage">{oneMes}</div>;
+}
+
+export default Message;

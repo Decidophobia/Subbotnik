3d47cb606006b3d52bd43b7f1839bb5f9dea5bea
AleksandrKocherga <senior.kocherga@gmail.com>
2020-11-22 17:39:26 +0300

    add custom placemark

--- client/src/Components/Map/Map.js
+++ client/src/Components/Map/Map.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import Modal from "react-modal";
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import {
   Map,
   Placemark,
   Clusterer,
   GeolocationControl,
-} from "react-yandex-maps";
-import ModalWindow from "../ModalWindow/ModalWindow.js";
-import styles from "./Map.module.css";
-import Chat from "../Chat/Chat";
+} from 'react-yandex-maps';
+import ModalWindow from '../ModalWindow/ModalWindow.js';
+import styles from './Map.module.css';
+import Chat from '../Chat/Chat';
 
-Modal.setAppElement("#root");
+Modal.setAppElement('#root');
 
 function MapPage(props) {
   //в этом стэйте массив с массивами координат
@@ -29,12 +29,12 @@ function MapPage(props) {
     <>
       <div className={styles.containerWrap}>
         <Map
-          width={"600px"}
-          height={"500px"}
+          width={'600px'}
+          height={'500px'}
           defaultState={{
             center: [59.94153469, 30.24667669],
             zoom: 13,
-            controls: ["zoomControl", "fullscreenControl"],
+            controls: ['zoomControl', 'fullscreenControl'],
           }}
           onClick={(e) => {
             // этот таймаут открывает модальное окно.
@@ -42,13 +42,13 @@ function MapPage(props) {
               return openModal();
             }, 500);
             //в coords прилетает масив с координатами.
-            const coords = e.get("coords");
+            const coords = e.get('coords');
             return setPlaceMark((prev) => [...prev, coords]);
           }}
         >
           <Clusterer
             options={{
-              preset: "islands#invertedVioletClusterIcons",
+              preset: 'islands#invertedVioletClusterIcons',
               groupByCoordinates: false,
             }}
           >
@@ -59,19 +59,23 @@ function MapPage(props) {
                   key={index}
                   geometry={coordinates}
                   properties={{
-                    iconContent: "Грязюка",
+                    // iconContent: 'Грязюка',
                     balloonContentHeader:
                       '<span class="description">Ваша отметка</span>',
                     balloonContentBody: `Туть грязно`,
                   }}
                   options={{
-                    preset: "islands#redStretchyIcon",
+                    iconLayout: 'default#image',
+                    iconImageHref: 'https://psv4.userapi.com/c856220/u8423482/docs/d4/a62869fdf7ee/placemark-06.png?extra=vfLVfiI9KJh8kPP143yaJMZHXyG1-PszB1QCpBhesI3Yo0CcPhYjkihP7JU7lZATowUotK2FMNFhmXsG-_vjU-mo3LtlPD6zBmatW_cmGr4PEIswQDlVTvla69SHqKK2BjWlVuKBSx4ojVHrrw',
+                    iconImageSize: [45, 60],
+                    iconImageOffset: [-20, -20],
+                    // preset: 'islands#redStretchyIcon',
                     // draggable: true,
                   }}
                 />
               ))}
           </Clusterer>
-          <GeolocationControl options={{ float: "left" }} />
+          <GeolocationControl options={{ float: 'left' }} />
         </Map>
         <Chat />
       </div>
